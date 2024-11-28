745b64659a31a6ebb2ae377c537e2628fc985a0d
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2024-11-28 16:04:28 +0600

    renamed studiocubics to cubicsui

--- src/functions/buildConfigFile.ts
+++ src/functions/buildConfigFile.ts
@@ -1,32 +1,38 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 
-// Default configuration template
-const defaultConfigTemplate = `
-import { defineConfig } from '@studiocubics/core';
-
-export default defineConfig({
-  // Your StudioCubics configuration
-  project: {
-    name: 'My StudioCubics Project',
-  },
+const defaultConfigs = `{
+  // Your Cubics UI configuration
+  styleEngine: "css",
+  type: "typescript",
   // Add more configuration options as needed
-});
+}`;
+
+// Default configuration templates
+const defaultConfigTemplateTS = `
+import { defineConfig } from '@cubicsui/cli';
+
+export default defineConfig(${defaultConfigs});
+`;
+const defaultConfigTemplateJS = `
+import { defineConfig } from '@cubicsui/cli';
+
+export default defineConfig(${defaultConfigs});
 `;
 
 export default function buildConfigFile() {
-  const configPath = path.resolve(process.cwd(), "sc.config.ts");
+  const configPath = path.resolve(process.cwd(), "cui.config.ts");
 
   // Check if config already exists
   if (fs.existsSync(configPath)) {
-    console.error("sc.config.ts already exists in this directory.");
+    console.error("cui.config.ts already exists in this directory.");
     process.exit(1);
   }
 
   // Write the default configuration file
   try {
-    fs.writeFileSync(configPath, defaultConfigTemplate.trim());
-    console.log("✔ Created sc.config.ts in the project root.");
+    fs.writeFileSync(configPath, defaultConfigTemplateTS.trim());
+    console.log("✔ Created cui.config.ts in the project root.");
   } catch (error) {
     console.error("✖ Failed to create sc.config.ts:", error);
     process.exit(1);
